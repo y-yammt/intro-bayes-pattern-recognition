2561108379641ed8f1f4b372fa30bee11b8695af
Yuji Yamamoto <y.yammt@gmail.com>
2014-09-26 13:19:46 +0900

    Initial commit.

---
+++ slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: '入門ベイズ統計',
+    subtitle: '第4章 ベイズ判別問題とパターン認識',
+    eventInfo: {
+      title: 'ベイズ統計読書会',
+      date: '09/26/2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Yuji Yamamoto',
+    company: '',
+    twitter: '@y_yammt',
+    github: 'https://github.com/y-yammt'
+  }
+  ]
+};
+
